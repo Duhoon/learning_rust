66efb1583ef6e4a65e5636cd2a4b9c92723b9d5a
Duhoon <eclip6@naver.com>
2023-09-14 04:26:24 +0900

    feat: Insertion Sort

---
+++ insertion_sort/src/lib.rs
@@ -0,0 +1,28 @@
+pub fn add(mut arr: Vec<usize> ) -> Vec<usize> {
+    let length : usize = arr.len();
+    
+    for i in 1..length {
+        let key = arr[i];
+        
+        let mut j = i - 1 ;
+        while j > 0 && arr[j] > key {
+            arr[j+1] = arr[j];
+            j = j - 1;
+        }
+        arr[j+1] = key;
+    }
+
+    arr
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let arr = vec![5,2,7,1,9,4,3];
+        let new_arr = add(arr);
+        println!("{:?}", new_arr);
+    }
+}
